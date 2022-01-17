857b999b6666aa0e7aca891348cffb38d9c0bb21
akykeung <akykeung@outlook.com>
2022-01-16 22:35:32 -0700

    Created database to store inventory data

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
 import streamlit as st
+import sqlite3
 
+#Database
+conn = sqlite3.connect('inventory.db')
+c = conn.cursor()
+
+# Functions
+
+#Creating a table
+def create_table():
+    c.execute('CREATE TABLE IF NOT EXISTS inventorytable(Inventory Name TEXT, Inventory Details TEXT, Creation Date DATE)')
+
+# Recieve or add table
+def add_data(Inventory Name, Inventory Details, Creation Date):
+    c.execute('INSERT INTO inventorytable(Inventory Name, Inventory Details, Creation Date) VALUES(?,?,?)',(Inventory Name, Inventory Details, Creation Date))
+    conn.commit()
+
+def view_all_inventory():
+    c.execute('SELECT * FROM inventorytable')
+    data = c.fetchall()
+    return data
 
 
 st.title("Inventory Tracking")
@@ -21,6 +41,8 @@ elif choice == "Add Inventory":
     inventory_name = st.text_input("Enter Inventory Name", max_chars=100)
     inventory_details = st.text_area("Enter Inventory Details", height=150)
     inventory_date = st.date_input("Date")
+    if st.button("Add"):
+        st.success("Submit:{} saved".format(inventory_name))
 
 elif choice == "Search":
     st.subheader("Search Inventory Items")
