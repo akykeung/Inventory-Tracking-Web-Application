10d85be55eb0d7060fec927f48e1ea15de5cb76a
akykeung <akykeung@outlook.com>
2022-01-19 01:44:53 -0700

    Added code with html templates

---
+++ html_template.py
@@ -0,0 +1,16 @@
+# Template on Layout for inventory items
+template = """
+<div style="background-color:#55aaaa;padding:10px;border-radius:10px;margin:20px;">
+<h4 style="color:white; text-align: center;">{}</h4>
+<p style = "text-align:center;">{}</p>
+<h6 style="color:white; text-align: center;">Creation Date: {}</h4>
+</div>
+"""
+
+hide_default_footer = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>
+
+"""
